5a230a1af27b849fe3f9993df7588b445ea66032
Jason van den Berg <jayvdb1@gmail.com>
2022-08-02 18:33:37 +0200

    Unit test

---
+++ __tests__/result.ts
@@ -0,0 +1,37 @@
+import {Result, ok, err} from '../src/index';
+
+describe('result check', () => {
+  it('returns result', async () => {
+    const func = (): Result<string> => {
+      return ok('result');
+    }
+
+    const result = func();
+
+    expect(result.isOk()).toBe(true);
+    expect(result.isErr()).toBe(false);
+
+    // @ts-ignore
+    expect(result.value).toBe('result');
+    // @ts-ignore
+    expect(result.error).not.toBeDefined();
+  });
+
+  it('returns error', async () => {
+    const func = (): Result<string> => {
+      return err('failed message');
+    }
+
+    const result = func();
+
+    expect(result.isOk()).toBe(false);
+    expect(result.isErr()).toBe(true);
+
+    // @ts-ignore
+    expect(result.value).not.toBeDefined();
+    // @ts-ignore
+    expect(result.error).toBeDefined();
+    // @ts-ignore
+    expect(result.error.message).toBe('failed message');
+  });
+});
