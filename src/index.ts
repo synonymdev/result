b8791e020df1ae439944f507ace2a4586dc2fb27
nazeh <ar.nazeh@gmail.com>
2023-07-27 13:02:03 +0300

    chore: add legacy tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 declare var __DEV__: boolean;
 
-export class Result<T, E> {
-  constructor(public readonly value: T, public readonly error: E) { }
+/**
+ * Construct a new Ok result value.
+ */
+export const ok = <T>(value: T): Result<T> => new Result(value);
+
+/**
+ * Construct a new Err result value.
+ */
+export const err = <T>(error: Error | string): Result<T> => {
+  if (typeof error === 'string') {
+    error = new Error(error)
+  }
+
+  // @ts-ignore
+  return new Result(undefined, error);
+};
+
+export class Result<T, E = Error> {
+  constructor(public readonly value: T, public readonly error?: E) { }
 
   isOk(): boolean {
     return this.error === undefined;
@@ -10,33 +27,9 @@ export class Result<T, E> {
   isErr(): boolean {
     return !this.isOk();
   }
+
+  static ok = ok;
+  static err = err;
 }
 
-export class Ok<T> extends Result<T, undefined>{
-  constructor(value: T) {
-    super(value, undefined)
-  }
-}
-
-export class Err<E> extends Result<undefined, E>{
-  constructor(error: E) {
-    super(undefined, error)
-  }
-}
-
-/**
- * Construct a new Ok result value.
- */
-export const ok = <T>(value: T): Result<T, undefined> => new Ok(value);
-
-/**
- * Construct a new Err result value.
- */
-export const err = <E>(error: Error | string): Err<E> => {
-  if (typeof error === 'string') {
-    error = new Error(error)
-  }
-
-  // @ts-ignore
-  return new Err(error);
-};
+export default Result
